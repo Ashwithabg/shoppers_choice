d275e7320d23b799b40538546aadc2ce026f01ac
Ashwitha <ashwitha.b.aux@go-jek.com>
2019-05-25 14:24:04 +0530

    use templates to render static html files

--- src/main.go
+++ src/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	templates := populatetemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+
+	http.ListenAndServe(":8000", nil)
+}
+
+func populatetemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
